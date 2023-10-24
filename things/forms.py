e3f577a808a18bcc15a95995c9a98baecf69ca28
RayPops <118989106+RayPops@users.noreply.github.com>
2023-10-24 22:19:06 +0000

    Add thing form functionality

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from django.core.validators import RegexValidator
+from things.models import Thing
+
+class ThingForm(forms.ModelForm):
+
+    class Meta:
+        model = Thing
+        fields = ('name', 'description', 'quantity')
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 5}),
+            'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50})
+        }
+    
+
+    def clean(self):
+        super().clean()
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if not password1 == password2:
+            raise forms.ValidationError('Passwords do not match.')
